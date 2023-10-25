f787cf1236bfc948be604ca7ee0a81e1f9e441fb
fazam0616 <fazam0616@hotmail.com>
2023-10-25 03:51:28 -0400

    Inital commit for mobile front end services

---
+++ BLinck for Android/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text, Image, ScrollView, TextInput} from 'react-native';
+
+
+const YourApp = () => {
+  return (
+      <ScrollView>
+        <View>
+          <Text>
+            Hola
+          </Text>
+        </View>
+      </ScrollView>
+  );
+};
+
+export default YourApp;
