226e68381bcd4ef8f0afea361fdf8b7642c762df
fazam0616 <fazam0616@hotmail.com>
2023-10-27 05:31:44 -0400

    Added account summary screen, transaction screen, account creation, wallet creation, and wallet viewing

--- BLinck for Android/App.tsx
+++ BLinck for Android/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import {View, Text, Image, ScrollView, TextInput, Button, ImageBackground, StyleSheet, Alert} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { authorize } from 'react-native-app-auth';
 import {
   GoogleSignin,
@@ -17,8 +18,6 @@ import User from 'hedera-sdk/User.js'
 // import { Client } from "@hashgraph/sdk";
 
 const fhandle = new FirebaseHandler();
-let currentUser = null;
-console.log(fhandle);
 const config = {
   issuer: 'https://accounts.google.com',
   clientId: '76329107601-20n9pm9sq7a1fj1hedokc2f4m1ms7s96.apps.googleusercontent.com',
@@ -26,8 +25,8 @@ const config = {
   scopes: ['openid', 'profile'],
 };
 
-const Stack = createNativeStackNavigator();
-const currentUser = null;
+
+
 async function signIn(nav){
    GoogleSignin.configure({
        androidClientId: '76329107601-20n9pm9sq7a1fj1hedokc2f4m1ms7s96.apps.googleusercontent.com',
@@ -41,15 +40,15 @@ async function signIn(nav){
                     //console.log(data);
                     let name=(data['user'])['name'];
                     let email=(data['user'])['email'];
-                    let token=data['idToken'];
+                    let token=data['idToken'];//NOT CORRECT BUT I DONT WANNA DO BASE64 DECODING (use "kid" value?)
                     // console.log("Userdata: "+userData);
                     //let {userEmail,last,first} = userData;
                     //currentUser = User(first+" "+last, userEmail,token,[]);
                     //currentUser.firebaseUpdateUser(fhandle);
-                    currentUser = new User("name", "email",token,[1,2]);
-                    currentUser.firebaseUpdateUser();
-
-                    nav.replace('Dashboard',{navigation:nav, userInfo:currentUser});
+                    currentUser = new User(name, email,token,[]);
+                    currentUser.firebaseUpdateUser(fhandle);
+                    //console.log(currentUser);
+                    nav.replace('Dashboard',{user:currentUser});
                 }).catch((e) => {
                 console.log("ERROR IS: " + JSON.stringify(e));
                 })
@@ -95,38 +94,102 @@ function fakeWallet(name, colour,amount){
 function InfoSummary({title, desc, amount}){
     return (
         <View style={styles.infoSummary} >
-            <View>
-                <Text style={styles.infoSummaryTitle}>{title}</Text>
-                <Text style={styles.infoSummaryDesc}>{desc}</Text>
-                <Text style={styles.infoSummaryAmount}>{'$'+desc}</Text>
-            </View>
+            <Text style={styles.infoSummaryTitle}>{title}</Text>
+            <Text style={styles.infoSummaryDesc}>{desc}</Text>
+            <Text style={styles.infoSummaryAmount}>{'$'+amount}</Text>
         </View>
     );
 }
 
-function Dashboard({route, navigation}) {
-    const {userInfo} = route['params'];
-    const wallets = [];
+function Transactions({route,navigation}) {
+    //The current user
+    const {user} = route.params
+    return (
+        <View>
+            <Text>PlaceHolder</Text>
+        </View>
+    );
+}
 
-    wallets.push(fakeWallet("Netlix","#2e2b30",'30'));
-    wallets.push(fakeWallet("Amazon","purple",'100'));
-    wallets.push(fakeWallet("Rent","blue",'1100'));
-
+function AddCard({route,navigation}) {
+    const {user} = route.params;
+    const [name, onChangeName] = React.useState('');
+    const [currId, onChangeCurrId] = React.useState('');
     return (
         <LinearGradient start={{ x: 0.4, y: 0.6 }} end={{ x: 1, y: 0 }}
         colors={['#573173', '#101011']} style={styles.dashboard}>
             <View>
-            <Text style={styles.dashboardName}>Oooga</Text>
+                <Text style={styles.addCardSubtitle}>{"Card Name"}</Text>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="My Card"
+                        placeholderTextColor="#aaaaaa"
+                        onChangeText={onChangeName}
+                        value={name}
+                      />
+                </View>
+                <Text style={styles.addCardSubtitle}>{"Currency Type"}</Text>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="USD"
+                        placeholderTextColor="#aaaaaa"
+                        onChangeText={onChangeCurrId}
+                        value={currId}
+                      />
+                </View>
+                <Button title="Add" onPress={() => {
+                    if (name&&currId) {
+                        user.createNewWallet(name,5,currId);
+                        user.firebaseUpdateUser(fhandle);
+                        navigation.goBack();
+                    }
+                }}/>
             </View>
-            <ScrollView horizontal={true}>
-                {wallets.map((item, i) => <Card title={item.title} colour={item.color} balance={item.balance}/>)}
-            </ScrollView>
+        </LinearGradient>
+    );
+}
+
+function AccountSummary({route, navigation}) {
+    const {user} = route.params;
+    
+    return (
+        <LinearGradient start={{ x: 0.4, y: 0.6 }} end={{ x: 1, y: 0 }}
+        colors={['#573173', '#101011']} style={styles.dashboard}>
+            <View>
+                <Text style={styles.dashboardName}>{user.name}</Text>
+            </View>
+            <View>
+                <ScrollView horizontal={true}>
+                    {user.wallets.map((item, i) => <Card title={item.alias} colour={"purple"} balance={item.balance}/>)}
+                </ScrollView>
+            </View>
+            <View style={styles.addCardContainer}>
+                <Button style={styles.addCardButton} title="Add Card!" onPress={() => navigation.navigate('AddCard',{user:user})} />
+            </View>
             <InfoSummary title='Total Balance' desc='$CAD' amount='100'/>
         </LinearGradient>
     );
 }
 
+function Dashboard({route, navigation}) {
+    const Tab = createBottomTabNavigator();
+    const {user} = route.params;
+    console.log(user);
+
+    return (
+        <Tab.Navigator>
+          <Tab.Screen name="Account Summary" component={AccountSummary} options={styles.tabOption} initialParams={{ user:user}}/>
+          <Tab.Screen name="Transactions" component={Transactions}  options={styles.tabOption} initialParams={{ user:user}}/>
+
+        </Tab.Navigator>
+    );/*Add a Tab.Screen for every tab within the dashboard*/
+}
+
 const YourApp = () => {
+    const Stack = createNativeStackNavigator();
+
     return (
       <NavigationContainer>
         <Stack.Navigator screenOptions={{headerShown: false}}>
@@ -137,6 +200,10 @@ const YourApp = () => {
           <Stack.Screen
             name="Dashboard"
             component={Dashboard}
+          />
+          <Stack.Screen
+            name="AddCard"
+            component={AddCard}
           />
         </Stack.Navigator>
       </NavigationContainer>
@@ -173,7 +240,6 @@ const styles = StyleSheet.create({
   card:{
       width:250,
       height:157,
-      position: 'absolute',
       flexDirection:'row',
       flex:1,
       margin:5,
@@ -210,22 +276,54 @@ const styles = StyleSheet.create({
       position:'relative',
       height:40,
       textShadowRadius: 10,
-      backgroundColor:'#25182e'
+      backgroundColor:'#25182e',
+      borderRadius:10,
+      flexDirection:'row',
+      display:'flex',
+      justifyContent:'space-between',
   },
   infoSummaryTitle:{
       color:'white',
       fontSize:28,
+      marginLeft:3,
   },
   infoSummaryDesc:{
       color:'white',
       fontSize:14,
-      position:'absolute'
+      top:0,
   },
   infoSummaryAmount:{
       color:'white',
       fontSize:20,
-      position:'absolute',
-      marginRight:1
+      // position:'absolute',
+      // right:5,
+      // top:6,
+  },
+  tabOption:{
+      headerShown: false
+  },
+  addCardButton:{
+      borderRadius: 20,
+  },
+  addCardContainer:{
+      width:300,
+      alignSelf:'center',
+      padding:5
+  },
+  input:{
+      fontSize:28,
+      color:'white',
+      backgroundColor:"#3c274a"
+  },
+  inputContainer:{
+      borderWidth:2,
+      borderColor:"#25182e",
+      margin:5
+  },
+  addCardSubtitle:{
+      color:'white',
+      fontSize:30,
+      marginLeft:7
   }
 });
 
